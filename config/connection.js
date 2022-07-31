41b5a3a8d97d33a7f26fea81337ee107a0aeb581
Schris <tovechris48@gmail.com>
2022-07-31 10:38:05 -0400

    connect-env

--- config/connection.js
+++ config/connection.js
@@ -12,19 +12,11 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 8000
+    dialectOptions: {
+      decimalNumbers: true,
+      port: 8000
+    }
   });
 };
-
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       dialectOptions: {
-//         decimalNumbers: true,
-//         port: 3306
-//       },
-//     });
 
 module.exports = sequelize;
